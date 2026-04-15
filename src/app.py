bf9075331afe613d248560ace7627aafff3d2cf4
ReddSpade <matheo.rodriguez@outlook.fr>
2026-04-15 17:16:15 +0200

    feat: configuration SECRET_KEY via env

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-key-insecure")
 
 
 @app.route("/", methods=["GET"])
